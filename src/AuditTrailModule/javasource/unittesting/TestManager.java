7411c9f2ca0a7d5b0bef0302c89e49c8e6387a59
Augusto Passalacqua <Augusto.Passalacqua@mendix.com>
2020-12-08 11:37:32 +0100

    Update UI to Atlas, update modules
    Updated UI Resources module to Atlas UI Resources.
    Updated Administration module to Appstore provided one.
    Updated layouts.
    Updated Unit testing module.

--- src/AuditTrailModule/javasource/unittesting/TestManager.java
+++ src/AuditTrailModule/javasource/unittesting/TestManager.java
@@ -57,8 +57,6 @@ public class TestManager
 		}
 	}
 
-	private static final String	TEST_MICROFLOW_PREFIX_2	= "UT_";
-
 	static final String	CLOUD_SECURITY_ERROR = "Unable to find JUnit test classes or methods. \n\n";
 	
 	private static TestManager	instance;
@@ -66,6 +64,8 @@ public class TestManager
 	
 	private static final Map<String, Object> emptyArguments = new HashMap<String, Object>();
 	private static final Map<String, Class<?>[]> classCache = new HashMap<String, Class<?>[]>();
+	
+	private IContext setupContext;
 
 	private String	lastStep;
 	
@@ -127,7 +127,13 @@ public class TestManager
 		if (Core.getMicroflowNames().contains(testSuite.getModule() + ".Setup")) {
 			try {
 				LOG.info("Running Setup microflow..");
-				Core.execute(Core.createSystemContext(), testSuite.getModule() + ".Setup", emptyArguments);
+				if (testSuite.getAutoRollbackMFs()) {
+					setupContext = Core.createSystemContext();
+					setupContext.startTransaction();
+					Core.execute(setupContext, testSuite.getModule() + ".Setup", emptyArguments);
+				} else {
+					Core.execute(Core.createSystemContext(), testSuite.getModule() + ".Setup", emptyArguments);
+				}
 			}
 			catch(Exception e) {
 				LOG.error("Exception during SetUp microflow: " + e.getMessage(), e);
@@ -138,18 +144,33 @@ public class TestManager
 
 	private void runMfTearDown(TestSuite testSuite) 
 	{
+		IContext tearDownContext = setupContext;
 		if (Core.getMicroflowNames().contains(testSuite.getModule() + ".TearDown")) {
 			try
 			{
 				LOG.info("Running TearDown microflow..");
-				Core.execute(Core.createSystemContext(), testSuite.getModule() + ".TearDown", emptyArguments);
+				if (tearDownContext == null) {
+					tearDownContext = Core.createSystemContext();
+				}
+				if (testSuite.getAutoRollbackMFs()) {
+					tearDownContext.startTransaction();
+				}
+				Core.execute(tearDownContext, testSuite.getModule() + ".TearDown", emptyArguments);
 			}
 			catch (Exception e)
 			{
-				LOG.error("Severe: exception in unittest TearDown microflow '" + testSuite.getModule() + ".Setup': " +e.getMessage(), e);
+				LOG.error("Severe: exception in unittest TearDown microflow '" + testSuite.getModule() + ".TearDown': " +e.getMessage(), e);
 				throw new RuntimeException(e);
 			}
 		}
+		
+		// Either we had a teardown a teardown or 
+		if (testSuite.getAutoRollbackMFs() && tearDownContext != null) {
+			tearDownContext.rollbackTransAction();
+		}
+		
+		// Make sure we clean setupContext after running this test/suite
+		setupContext = null;
 	}
 	
 	public synchronized void runTestSuites() throws CoreException {
@@ -313,9 +334,18 @@ public class TestManager
 
 		commitSilent(test);
 
-		IContext mfContext = Core.createSystemContext();
-		if (testSuite.getAutoRollbackMFs())
+		IContext mfContext = null;
+		
+		if (testSuite.getAutoRollbackMFs()) {
+			if (Core.getMicroflowNames().contains(testSuite.getModule() + ".Setup"))
+				mfContext = setupContext.clone();
+			else
+				mfContext = Core.createSystemContext();
 			mfContext.startTransaction();
+		} else {
+			mfContext = Core.createSystemContext();
+		}
+		
 
 		long start = System.currentTimeMillis();
 
@@ -334,6 +364,8 @@ public class TestManager
 			return res;
 		}
 		catch(Exception e) {
+			if (testSuite.getAutoRollbackMFs())
+				mfContext.startTransaction();
 			start = System.currentTimeMillis() - start;
 			test.setResult(UnitTestResult._2_Failed);
 			Throwable cause = ExceptionUtils.getRootCause(e);
@@ -347,7 +379,6 @@ public class TestManager
 		finally {
 			if (testSuite.getAutoRollbackMFs())
 				mfContext.rollbackTransAction();
-				
 			test.setLastStep(lastStep);
 			test.setReadableTime((start > 10000 ? Math.round(start / 1000) + " seconds" : start + " milliseconds"));
 			commitSilent(test);
@@ -424,7 +455,6 @@ public class TestManager
 		
 		ZipFile zipFile = new ZipFile(projectJar);
 		Enumeration<? extends ZipEntry> entries = zipFile.entries();
-		System.out.println("Starting processProjec");
 		while(entries.hasMoreElements()){
 			ZipEntry zipEntry = entries.nextElement();
 			String fileName = zipEntry.getName();
@@ -600,5 +630,4 @@ public class TestManager
 		//MWE: this system is problematic weird if used from multiple simultanously used threads..
 		return lastStep;
 	}
-	
 }
