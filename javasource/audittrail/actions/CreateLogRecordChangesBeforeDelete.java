67d93d2999515eca839ff7d2cb210fe13974b736
Augusto Passalacqua <Augusto.Passalacqua@mendix.com>
2023-04-21 14:49:45 +0200

    Update action files with ones generated by mxBuild

--- javasource/audittrail/actions/CreateLogRecordChangesBeforeDelete.java
+++ javasource/audittrail/actions/CreateLogRecordChangesBeforeDelete.java
@@ -45,6 +45,7 @@ public class CreateLogRecordChangesBeforeDelete extends CustomJavaAction<IMendix
 
 	/**
 	 * Returns a string representation of this action
+	 * @return a string representation of this action
 	 */
 	@java.lang.Override
 	public java.lang.String toString()
--- javasource/audittrail/actions/CreateLogRecordOfObject.java
+++ javasource/audittrail/actions/CreateLogRecordOfObject.java
@@ -49,6 +49,7 @@ public class CreateLogRecordOfObject extends CustomJavaAction<IMendixObject>
 
 	/**
 	 * Returns a string representation of this action
+	 * @return a string representation of this action
 	 */
 	@java.lang.Override
 	public java.lang.String toString()
