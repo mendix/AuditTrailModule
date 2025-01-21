a4eb672a42d3bbf314d11fe37ef4c2cd59a0c6c4
Pavel Antoshin <pavel.antoshin@mendix.com>
2025-01-21 17:01:46 +0100

    Add changes in javasource that are generated on deployment

--- javasource/system/UserActionsRegistrar.java
+++ javasource/system/UserActionsRegistrar.java
@@ -6,7 +6,6 @@ public class UserActionsRegistrar
 {
   public void registerActions(IActionRegistrator registrator)
   {
-    registrator.bundleComponentLoaded();
     registrator.registerUserAction(audittrail.actions.CreateLogRecordChangesBeforeDelete.class);
     registrator.registerUserAction(audittrail.actions.CreateLogRecordOfObject.class);
     registrator.registerUserAction(audittrail.actions.GetDiff.class);
